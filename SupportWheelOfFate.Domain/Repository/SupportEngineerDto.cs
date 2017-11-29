5c84845b9e188eaef52820ee08fce6361e141fc4
Piotr Polanski <polanski.piotr@me.com>
2017-11-29 18:56:07 +0100

    move SupportEngineerDto to it's own file, rename SupportEngineerDtoes table to SupportEngineers

---
+++ SupportWheelOfFate.Domain/Repository/SupportEngineerDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using SupportWheelOfFate.Domain.Model;
+
+namespace SupportWheelOfFate.Domain.Repository
+{
+    [Table("SupportEngineers")]
+    public class SupportEngineerDto
+    {
+        public SupportEngineerDto()
+        {
+            
+        }
+        public SupportEngineerDto(string name, ICollection<Shift> shiftLog)
+        {
+            Name = name;
+            ShiftLog = shiftLog;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Shift> ShiftLog { get; set; }
+    }
+}
--- SupportWheelOfFate.Domain/Repository/SupportEngineersRepository.cs
+++ SupportWheelOfFate.Domain/Repository/SupportEngineersRepository.cs
@@ -2,7 +2,6 @@
 using System.Data.Entity;
 using System.Linq;
 using SupportWheelOfFate.Domain.Abstract;
-using SupportWheelOfFate.Domain.Model;
 
 namespace SupportWheelOfFate.Domain.Repository
 {
@@ -25,22 +24,5 @@ namespace SupportWheelOfFate.Domain.Repository
         {
             _wheelOfFateContext.SaveChanges();
         }
-    }
-
-    public class SupportEngineerDto
-    {
-        public SupportEngineerDto()
-        {
-            
-        }
-        public SupportEngineerDto(string name, ICollection<Shift> shiftLog)
-        {
-            Name = name;
-            ShiftLog = shiftLog;
-        }
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Shift> ShiftLog { get; set; }
     }
 }
